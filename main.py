9d6f3accded05a80b3287ff7a7dc494e25f11e66
James Boggs <jmsboggs@gmail.com>
2022-09-07 15:22:42 -0400

    Add command line arguments and clean up main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from argparse import ArgumentParser
+from pathlib import Path
 from subprocess import Popen
 import sys
 
@@ -6,30 +8,77 @@ import pysoarlib as psl
 from agent_connector import AgentConnector, StateViewerConnector
 from gui import MineSoarGUI
 
-MALMO_PORT = 9000
+
 USER_NAME = "boggsj"
-MALMO_ARGS = [f"/home/{USER_NAME}/Coding/malmo/Minecraft/launchClient.sh", "-port", str(MALMO_PORT), "-env"]
 
-if __name__ == "__main__":
+#######################
+# PARSE CLI ARGUMENTS #
+#######################
+cli = ArgumentParser(description="Test bed for Soar agents with SVS 2.0 running in the Malmo environment.")
+cli.add_argument(
+    "-p", "--port", 
+    default=9000,
+    type=int,
+    help="The port to use for Python <-> Malmo communication."
+)
+cli.add_argument(
+    "-a", "--agent", 
+    default="atari_agents/agent_gamma.soar",
+    type=Path,
+    help="The path to the .soar file which should be sourced for the agent."
+)
+cli.add_argument(
+    "-n", "--name",
+    default="Steve",
+    type=str,
+    help="The name for the agent in Minecraft."
+)
+cli.add_argument(
+    "-w", "--watch-level",
+    default=1,
+    type=int,
+    choices=[0, 1, 2, 3, 4 ,5],
+    help="The Soar watch level, controls how verbose Soar output is."
+)
+cli.add_argument(
+    "-l", "--launch-minecraft",
+    action="store_true",
+    help="Flag that indicates the minecraft client should be launched along with the python app."
+)
+cli_namespace = cli.parse_args()
 
-    agent = psl.SoarClient(agent_name="Steve",
-                           agent_source="/home/boggsj/Coding/research/mine_soar/atari_agents/agent_gamma.soar",
-                           write_to_stdout=True,
-                           watch_level=1)
+######################
+# CREATE SOAR CLIENT #
+######################
+agent = psl.SoarClient(agent_name=cli_namespace.name,
+                        agent_source=str(cli_namespace.agent),
+                        write_to_stdout=True,
+                        watch_level=cli_namespace.watch_level)
 
-    minecraft_connector = AgentConnector(agent)
-    minecraft_connector.add_output_command("take-action")
-    agent.add_connector("minecraft", minecraft_connector)
+minecraft_connector = AgentConnector(agent)
+minecraft_connector.add_output_command("take-action")
+agent.add_connector("minecraft", minecraft_connector)
 
-    state_view_connector = StateViewerConnector(agent)
-    agent.add_connector("state_viewer", state_view_connector)
+state_view_connector = StateViewerConnector(agent)
+agent.add_connector("state_viewer", state_view_connector)
 
+##############
+# CREATE GUI #
+##############
+mine_gui = MineSoarGUI(agent, minecraft_connector, state_view_connector, cli_namespace.port)
+minecraft_connector.gui = mine_gui
+state_view_connector.gui = mine_gui
+agent.print_handler = mine_gui._soar_output_callback
+
+#######################
+# LAUNCH MALMO CLIENT #
+#######################
+if cli_namespace.launch_minecraft:
+    MALMO_ARGS = [f"/home/{USER_NAME}/Coding/malmo/Minecraft/launchClient.sh", "-port", str(cli_namespace.port), "-env"]
     malmo_proc = Popen(MALMO_ARGS, stdout=sys.stdout)
-    
-    mine_gui = MineSoarGUI(agent, minecraft_connector, state_view_connector, MALMO_PORT)
-    minecraft_connector.gui = mine_gui
-    state_view_connector.gui = mine_gui
-    agent.print_handler = mine_gui._soar_output_callback
-    
-    agent.connect()
-    mine_gui.mainloop()
+
+#################
+# PROGRAM START #
+#################
+agent.connect()
+mine_gui.mainloop()
